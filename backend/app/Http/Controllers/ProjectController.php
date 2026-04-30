8c9b44dfe26d80c0bd3551cfd3dad0d1288c6927
arnielmontero <arnielmontero@gmail.com>
2026-04-30 14:18:02 +0800

    added code

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -18,11 +18,19 @@ class ProjectController extends Controller
     } 
 
     public function update(Request $request, Project $project) {
+        if ($project->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Forbidden'], 403);
+        }
+
         $project->update($request->all()); 
         return response()->json($project); 
     } 
 
     public function destroy(Project $project) {
+        if ($project->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Forbidden'], 403);
+        }
+
         $project->delete(); 
         
         return response()->json(null, 204); 
